0bfffa2a335ec07425074cd05e2d8f11d0b1ae3d
winteriscomingpinball <silentcatalyst@gmail.com>
2016-07-31 20:27:33 -0400

    Arduino Mega Keyboard Sketch

---
+++ kbd_usb_input_mega2560/README.md
@@ -0,0 +1,6 @@
+This arduino sketch can be loaded onto an Arduino Mega2560 to turn it into a HID Keyboard.
+
+The onboard USB firmware has to be flashed to a HID firmware.  The firmware and additional information can be downloaded from:
+https://github.com/harlequin-tech/arduino-usb
+
+Make sure to put the device in DFU mode for flashing the firmware by momentarily bridging the 2 pins closest to the USB port (in the 6-pin header).
--- kbd_usb_input_mega2560/kbd_usb_input_mega2560.ino
+++ kbd_usb_input_mega2560/kbd_usb_input_mega2560.ino
@@ -1,1 +1,57 @@
+/* Arduino USB Keyboard HID demo
+*
+* Sends Volume up, hello world, and Volume Down to the host PC
+*
+*/
 
+/* Author: Darran Hunt
+ * Released into the public domain.
+ */
+
+uint8_t buf[8] = { 0 };	/* Keyboard report buffer */
+int pin;
+
+void setup();
+void loop();
+
+#define KEY_LEFT_CTRL	0x01
+#define KEY_LEFT_SHIFT	0x02
+#define KEY_RIGHT_CTRL	0x10
+#define KEY_RIGHT_SHIFT	0x20
+
+
+
+
+void setup()
+{
+    Serial.begin(9600);
+    delay(200);
+
+
+    for (pin=2;pin<54;pin=pin+1){
+      pinMode(pin,INPUT_PULLUP);
+    }
+}
+
+void loop()
+{
+  int i;
+
+  //clear buffer
+  for (i = 2; i < 8; i = i + 1) {
+     buf[i]=0;
+  }
+  i=2;
+
+  for (pin=2;pin<54;pin=pin+1){
+      pinMode(pin,INPUT_PULLUP);
+      if( digitalRead(pin) == 0){
+          buf[i] = pin+5;
+          if (i<8){
+            i++;
+          }
+        }
+    }
+
+Serial.write(buf, 8); // Send keypress
+}
