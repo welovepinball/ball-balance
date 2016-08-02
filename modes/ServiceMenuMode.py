325bb7aab4d5be197710f80f6f2c59d7b1060657
winteriscomingpinball <silentcatalyst@gmail.com>
2016-08-02 12:59:49 -0400

    Currently mapped keys now show

--- modes/ServiceMenuMode.py
+++ modes/ServiceMenuMode.py
@@ -23,28 +23,28 @@ config_menu_items = []
 global config_menu_current_switch
 
 
-config_menu_items.append("LEFT_JOY_UP:" + pygame.key.name(c.LEFT_JOY_UP) +":"+ str(c.LEFT_JOY_UP))
-config_menu_items.append("LEFT_JOY_DOWN:" + pygame.key.name(c.LEFT_JOY_DOWN) +":"+ str(c.LEFT_JOY_DOWN))
-config_menu_items.append("RIGHT_JOY_UP:" + pygame.key.name(c.RIGHT_JOY_UP) +":"+ str(c.RIGHT_JOY_UP))
-config_menu_items.append("RIGHT_JOY_DOWN:" + pygame.key.name(c.RIGHT_JOY_DOWN) +":"+ str(c.RIGHT_JOY_DOWN))
-config_menu_items.append("LEFT_LIMIT_TOP:" + pygame.key.name(c.LEFT_LIMIT_TOP) +":"+ str(c.LEFT_LIMIT_TOP))
-config_menu_items.append("LEFT_LIMIT_BOTTOM:" + pygame.key.name(c.LEFT_LIMIT_BOTTOM) +":"+ str(c.LEFT_LIMIT_BOTTOM))
-config_menu_items.append("RIGHT_LIMIT_TOP:" + pygame.key.name(c.RIGHT_LIMIT_TOP) +":"+ str(c.RIGHT_LIMIT_TOP))
-config_menu_items.append("RIGHT_LIMIT_BOTTOM:" + pygame.key.name(c.RIGHT_LIMIT_BOTTOM) +":"+ str(c.RIGHT_LIMIT_BOTTOM))
-config_menu_items.append("START_BUTTON:" + pygame.key.name(c.START_BUTTON) +":"+ str(c.START_BUTTON))
-config_menu_items.append("SERVICE_BUTTON:" + pygame.key.name(c.SERVICE_BUTTON) +":"+ str(c.SERVICE_BUTTON))
-config_menu_items.append("TILT_SWITCH:" + pygame.key.name(c.TILT_SWITCH) +":"+ str(c.TILT_SWITCH))
-config_menu_items.append("HOLE_1_SWITCH:" + pygame.key.name(c.HOLE_1_SWITCH) +":"+ str(c.HOLE_1_SWITCH))
-config_menu_items.append("HOLE_2_SWITCH:" + pygame.key.name(c.HOLE_2_SWITCH) +":"+ str(c.HOLE_2_SWITCH))
-config_menu_items.append("HOLE_3_SWITCH:" + pygame.key.name(c.HOLE_3_SWITCH) +":"+ str(c.HOLE_3_SWITCH))
-config_menu_items.append("HOLE_4_SWITCH:" + pygame.key.name(c.HOLE_4_SWITCH) +":"+ str(c.HOLE_4_SWITCH))
-config_menu_items.append("HOLE_5_SWITCH:" + pygame.key.name(c.HOLE_5_SWITCH) +":"+ str(c.HOLE_5_SWITCH))
-config_menu_items.append("HOLE_6_SWITCH:" + pygame.key.name(c.HOLE_6_SWITCH) +":"+ str(c.HOLE_6_SWITCH))
-config_menu_items.append("HOLE_7_SWITCH:" + pygame.key.name(c.HOLE_7_SWITCH) +":"+ str(c.HOLE_7_SWITCH))
-config_menu_items.append("HOLE_8_SWITCH:" + pygame.key.name(c.HOLE_8_SWITCH) +":"+ str(c.HOLE_8_SWITCH))
-config_menu_items.append("HOLE_9_SWITCH:" + pygame.key.name(c.HOLE_9_SWITCH) +":"+ str(c.HOLE_9_SWITCH))
-config_menu_items.append("HOLE_10_SWITCH:" + pygame.key.name(c.HOLE_10_SWITCH) +":"+ str(c.HOLE_10_SWITCH))
-config_menu_items.append("HOLE_FAILURE_SWITCH:" + pygame.key.name(c.HOLE_FAILURE_SWITCH) +":"+ str(c.HOLE_FAILURE_SWITCH))
+config_menu_items.append("LEFT_JOY_UP" + '\n' + "Current Key: " + c.key_name_lookup(c.LEFT_JOY_UP) + '\n' + "Current Value: "+ str(c.LEFT_JOY_UP))
+config_menu_items.append("LEFT_JOY_DOWN" + '\n' + "Current Key: " + c.key_name_lookup(c.LEFT_JOY_DOWN) + '\n' + "Current Value: "+ str(c.LEFT_JOY_DOWN))
+config_menu_items.append("RIGHT_JOY_UP" + '\n' + "Current Key: " + c.key_name_lookup(c.RIGHT_JOY_UP) + '\n' + "Current Value: "+ str(c.RIGHT_JOY_UP))
+config_menu_items.append("RIGHT_JOY_DOWN" + '\n' + "Current Key: " + c.key_name_lookup(c.RIGHT_JOY_DOWN) + '\n' + "Current Value: "+ str(c.RIGHT_JOY_DOWN))
+config_menu_items.append("LEFT_LIMIT_TOP" + '\n' + "Current Key: " + c.key_name_lookup(c.LEFT_LIMIT_TOP) + '\n' + "Current Value: "+ str(c.LEFT_LIMIT_TOP))
+config_menu_items.append("LEFT_LIMIT_BOTTOM" + '\n' + "Current Key: " + c.key_name_lookup(c.LEFT_LIMIT_BOTTOM) + '\n' + "Current Value: "+ str(c.LEFT_LIMIT_BOTTOM))
+config_menu_items.append("RIGHT_LIMIT_TOP" + '\n' + "Current Key: " + c.key_name_lookup(c.RIGHT_LIMIT_TOP) + '\n' + "Current Value: "+ str(c.RIGHT_LIMIT_TOP))
+config_menu_items.append("RIGHT_LIMIT_BOTTOM" + '\n' + "Current Key: " + c.key_name_lookup(c.RIGHT_LIMIT_BOTTOM) + '\n' + "Current Value: "+ str(c.RIGHT_LIMIT_BOTTOM))
+config_menu_items.append("START_BUTTON" + '\n' + "Current Key: " + c.key_name_lookup(c.START_BUTTON) + '\n' + "Current Value: "+ str(c.START_BUTTON))
+config_menu_items.append("SERVICE_BUTTON" + '\n' + "Current Key: " + c.key_name_lookup(c.SERVICE_BUTTON) + '\n' + "Current Value: "+ str(c.SERVICE_BUTTON))
+config_menu_items.append("TILT_SWITCH" + '\n' + "Current Key: " + c.key_name_lookup(c.TILT_SWITCH) + '\n' + "Current Value: "+ str(c.TILT_SWITCH))
+config_menu_items.append("HOLE_1_SWITCH" + '\n' + "Current Key: " + c.key_name_lookup(c.HOLE_1_SWITCH) + '\n' + "Current Value: "+ str(c.HOLE_1_SWITCH))
+config_menu_items.append("HOLE_2_SWITCH" + '\n' + "Current Key: " + c.key_name_lookup(c.HOLE_2_SWITCH) + '\n' + "Current Value: "+ str(c.HOLE_2_SWITCH))
+config_menu_items.append("HOLE_3_SWITCH" + '\n' + "Current Key: " + c.key_name_lookup(c.HOLE_3_SWITCH) + '\n' + "Current Value: "+ str(c.HOLE_3_SWITCH))
+config_menu_items.append("HOLE_4_SWITCH" + '\n' + "Current Key: " + c.key_name_lookup(c.HOLE_4_SWITCH) + '\n' + "Current Value: "+ str(c.HOLE_4_SWITCH))
+config_menu_items.append("HOLE_5_SWITCH" + '\n' + "Current Key: " + c.key_name_lookup(c.HOLE_5_SWITCH) + '\n' + "Current Value: "+ str(c.HOLE_5_SWITCH))
+config_menu_items.append("HOLE_6_SWITCH" + '\n' + "Current Key: " + c.key_name_lookup(c.HOLE_6_SWITCH) + '\n' + "Current Value: "+ str(c.HOLE_6_SWITCH))
+config_menu_items.append("HOLE_7_SWITCH" + '\n' + "Current Key: " + c.key_name_lookup(c.HOLE_7_SWITCH) + '\n' + "Current Value: "+ str(c.HOLE_7_SWITCH))
+config_menu_items.append("HOLE_8_SWITCH" + '\n' + "Current Key: " + c.key_name_lookup(c.HOLE_8_SWITCH) + '\n' + "Current Value: "+ str(c.HOLE_8_SWITCH))
+config_menu_items.append("HOLE_9_SWITCH" + '\n' + "Current Key: " + c.key_name_lookup(c.HOLE_9_SWITCH) + '\n' + "Current Value: "+ str(c.HOLE_9_SWITCH))
+config_menu_items.append("HOLE_10_SWITCH" + '\n' + "Current Key: " + c.key_name_lookup(c.HOLE_10_SWITCH) + '\n' + "Current Value: "+ str(c.HOLE_10_SWITCH))
+config_menu_items.append("HOLE_FAILURE_SWITCH" + '\n' + "Current Key: " + c.key_name_lookup(c.HOLE_FAILURE_SWITCH) + '\n' + "Current Value: "+ str(c.HOLE_FAILURE_SWITCH))
 
 global new_keys
 new_keys = []
@@ -74,7 +74,7 @@ class ServiceMenuMode(tools.ModeBase):
         for event in events:
             if event.type == pygame.KEYDOWN:
                 if selected_menu == main_menu_items[0]:
-                    print (event.key)
+                    #print (event.key)
                     new_key_check = 1
                     if config_menu_current_switch > 0:
                         for key in new_keys:
@@ -267,7 +267,7 @@ class ServiceMenuMode(tools.ModeBase):
             
             # Display instructional text under previous entry - edit font size and color in the next 2 lines
             font = pygame.font.Font(None, 25)
-            text = font.render("Active the switch for:", 1, (255, 255, 0))
+            text = font.render("Activate the switch for:", 1, (255, 255, 0))
             textpos = text.get_rect()
             textpos.top = (prevpos.bottom+3)
             textpos.left  = 50
@@ -278,10 +278,35 @@ class ServiceMenuMode(tools.ModeBase):
 
             #Loop through and display all config menu items
 
+            #parse out separate strings from config_menu_items
+            str_part1 = config_menu_items[config_menu_current_switch]
+            str_part2 = str_part1[str_part1.find('\n'):]
+            str_part1 = str_part1[:str_part1.find('\n')]
+            str_part2 = str_part2[str_part2.find('\n')+1:]
+            str_part3 = str_part2[str_part2.find('\n')+1:]
+            str_part2 = str_part2[:str_part2.find('\n')]
             
+            #print ("string 1: " + str_part1)
+            #print ("string 2: " +str_part2)
+            #print ("string 3: " +str_part3)
+
+            #display separate strings in meaningful way
             font = pygame.font.Font(None, 25)
-            text = font.render(config_menu_items[config_menu_current_switch], 1, (255, 255, 0))
-            
+            text = font.render(str_part1, 1, (255, 255, 0))
+            textpos = text.get_rect()
+            textpos.top = (prevpos.bottom+5)
+            textpos.left  = 50
+            background.blit(text, textpos)
+            prevpos = textpos
+
+            text = font.render(str_part2, 1, (0, 255, 0))
+            textpos = text.get_rect()
+            textpos.top = (prevpos.bottom+5)
+            textpos.left  = 50
+            background.blit(text, textpos)
+            prevpos = textpos
+
+            text = font.render(str_part3, 1, (0, 255, 0))
             textpos = text.get_rect()
             textpos.top = (prevpos.bottom+5)
             textpos.left  = 50
